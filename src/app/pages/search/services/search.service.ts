4701035b313b2fd9415c0a848f8f41f58a641af5
Kelvin Silva <kelvin.silva@gmail.com>
2022-03-03 22:31:00 -0300

    All Services API call

--- src/app/pages/search/services/search.service.ts
+++ src/app/pages/search/services/search.service.ts
@@ -13,4 +13,16 @@ export class SearchService {
   carregarRegistroSelect(name: string): Observable<any> {
     return this.http.get(env.baseApiUrl + this.PATH + '?name=' + name);
   }
+  carregarCandidato(name: string): Observable<any> {
+    return this.http.get(env.baseApiUrl + this.PATH + '/' + name);
+  }
+  setLikeCandidato(name: string): Observable<any> {
+    return this.http.put(env.baseApiUrl + this.PATH + '/' + name + '/like' ,{} );
+  }
+  setUnLikeCandidato(name: string): Observable<any> {
+    return this.http.put(env.baseApiUrl + this.PATH + '/' + name + '/unlike', {}  );
+  }
+  saveComment(name: string, comentario: string): Observable<any> {
+    return this.http.post(env.baseApiUrl + this.PATH + '/' + name + '/comment', {comentario: comentario}   );
+  }
 }
