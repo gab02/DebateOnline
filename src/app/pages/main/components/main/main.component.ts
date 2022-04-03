932c4fdc8e9fe6ceed0495e9d569ff3dc20fab6a
Kelvin Silva <kelvin.silva@gmail.com>
2022-04-03 02:22:50 -0300

    Alteracoes Layout

--- src/app/pages/main/components/main/main.component.ts
+++ src/app/pages/main/components/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Router } from '@angular/router';
+import { SearchService } from 'src/app/pages/search';
+import { Candidatos } from 'src/app/pages/search/models/candidatos';
 
 @Component({
   templateUrl: './main.component.html',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponente implements OnInit {
 
-  constructor() { }
+  findebar: string = '';
+  length: number;
+  pageSize = 10;
+  pgIndex = 0;
+  currentPage: number = 0;
+  pageEvent: PageEvent;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  candidatos: Candidatos[] = [];
+  candidato: Candidatos;
+
+  constructor(public service: SearchService, public router: Router) { }
 
   ngOnInit(): void {
-    console.log('Chamou o component')
+    this.currentPage = 1;
+    this.populate();
+  }
+
+  populate() {
+    this.service
+      .carregarRegistroSelect(this.findebar, this.currentPage, this.pageSize)
+      .subscribe((data) => {
+        this.candidatos = data.candidatos;
+        this.length = data.totalPages;
+        console.log(data);
+      });
+  }
+  getProfile(item: Candidatos) {
+    this.router.navigate(['profile'], {
+      state: { data: item },
+    });
+  }
+  onPaginateChange(event: PageEvent) {
+    this.currentPage = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.populate();
+  }
+
+  perfil(item: Candidatos) {
+    this.service.carregarCandidato(item._id).subscribe((data) => {
+      this.candidato = data;
+    });
   }
 
 }
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -3,13 +3,41 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MainComponent } from './components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     RouterModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    InputTextModule,
+    MatDividerModule,
+    MatListModule,
+
+    MatPaginatorModule,
+    MatDialogModule,
+    MatChipsModule,
     ],
   declarations: [
     MainComponent
